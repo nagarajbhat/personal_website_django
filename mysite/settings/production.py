3d28d92d589ef38c2ba00baa93d2205d9cb71b83
Nagaraj Bhat <nagarajbhat12@gmail.com>
2016-08-04 08:16:03 +0530

    small changes

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -27,8 +27,9 @@ ALLOWED_HOSTS = ['*']
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] =  dj_database_url.config()
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 
 
